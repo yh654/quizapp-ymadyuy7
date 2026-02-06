f10a8929149305f2127a975a3737ab7b649824cc
yh654 <evecan0311@gmail.com>
2026-02-07 00:12:55 +0900

    問題追加

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>import "./app.css";</script><main>
+  <!-- メニューバー -->
+  <div class="bg-red-200 flex justify-around text-x1 font-bold p-3">
+      <div>スコア:0</div>
+      <div>連続正解数:0</div>
+  </div>
+
+  <!-- 問題 -->
+  <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
+  <div class="bg-blue-200">
+      <button>選択肢1</button>
+      <button>選択肢2</button>
+      <button>選択肢3</button>
+  </div>
+</main>
